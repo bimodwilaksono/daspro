141a5ce0d0394a023d1ae79c3add3378d73e5197
Bimo Laksono <bimodwilaksono@gmail.com>
2025-09-21 20:11:37 +0700

    Tugas 1: Data Type in c++

---
+++ tugas_1/data_type.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string firstName = "Bimo ";
+  char middleName = 'D';
+  string lastName = " Laksono";
+  cout <<"Name : "<< firstName << middleName << lastName<< endl;
+
+  string NIM = "A18.2025.00201";
+  cout << "NIM : " << NIM;
+  
+  int age = 28;
+  cout << "Umur : " << age;
+
+  bool isMarried = false;
+  cout << "Sudah Menikah : " << isMarried;
+  
+  double gaji = 1000000000;
+  cout << "Gaji : " << gaji;
+
+  float g = 9.81;
+  cout << "percepatan gravitasi: " << g;
+
+  return 0;
+}
